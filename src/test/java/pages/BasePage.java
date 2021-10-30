ff6185dd68be7b7f3a89b1a19fc0ebe24e791bf4
Sofia Di Nonno <sofidinonno@gmail.com>
2021-10-30 14:51:09 -0300

    Create BasePage file and his methods

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,85 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected void navigate(String url){
+        driver.navigate().to(url);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+    }
+
+    protected WebElement findElement(By locator){
+        try {
+            WebElement element = driver.findElement(locator);
+            return element;
+        }
+        catch (NoSuchElementException e){
+            System.out.println("Element not found: " + locator);
+            throw new NoSuchElementException(e.getMessage());
+        }
+    }
+
+    protected List<WebElement> findElements(By locator){
+        try {
+            List<WebElement> element = driver.findElements(locator);
+            return element;
+        }
+        catch (NoSuchElementException e){
+            System.out.println("Elements not found: " + locator);
+            throw new NoSuchElementException(e.getMessage());
+        }
+    }
+
+    protected void click(By locator){
+
+        WebElement element = findElement(locator);
+
+        try{
+            if(element.isDisplayed())
+                element.click();
+
+        }
+        catch (Exception e) {
+            System.out.println("Error trying to click element.");
+            throw new NoSuchElementException(e.getMessage());
+        }
+    }
+
+    protected void inputText(By locator, String text){
+        try {
+            findElement(locator).sendKeys(text);
+        }
+        catch (Exception e) {
+            System.out.println("Error trying to enter text.");
+            throw new NoSuchElementException(e.getMessage());
+        }
+    }
+
+    protected void switchTo(String window){
+        driver.switchTo().window(window);
+    }
+
+    protected String getWindowHandle(){
+        return driver.getWindowHandle();
+    }
+
+    protected Set<String> getWindowHandles(){
+        return driver.getWindowHandles();
+    }
+
+
+}
